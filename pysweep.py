03fbbf4e292b9ab7fc38672ff71eb85ae0ed0936
Anthony Walker <walkanth@oregonstate.edu>
2019-06-24 14:00:27 -0700

    Created 3rd node in virtual cluster. Decided to use PyMP for node based parallelism and MPI for communication between nodes. Working on spliting data up

--- pysweep.py
+++ pysweep.py
@@ -13,45 +13,42 @@ import numpy as np
 from collections import deque
 
 #Parallel programming imports
-import pycuda.driver as cuda
-import pycuda.autoinit
-from pycuda.compiler import SourceModule
 try:
-    import GPUtil
+   import pycuda.driver as cuda
+   import pycuda.autoinit
+   from pycuda.compiler import SourceModule
+
 except:
-    pass
-import dev
+   pass
+
+import GPUtil
 from mpi4py import MPI
 import multiprocessing as mp
+import pymp
 #Testing imports
 import platform
+
+
 
 def sweep(y0, dy, t0, t_b, dt,block_size,ops,devices,gpu_affinity):
     """Use this function to perform swept rule>"""
     #-------------MPI Set up----------------------------#
     comm = MPI.COMM_WORLD
-
     master_rank = 0 #master rank
     num_ranks = comm.Get_size() #number of ranks
     rank = comm.Get_rank()  #current rank
-    print("size is ",num_ranks)
+    print("Rank: ",rank," Platform: ",platform.uname()[1])
 
-    universe_size=comm.Get_attr(MPI.UNIVERSE_SIZE)
-    print("universe size is ",universe_size)
-    print(platform.uname())
-    # univ_size = comm.Get_attr(MPI.UNIVERSE_SIZE)
-    # #Set up of dimensions
-    # plane_shape = np.shape(y0)  #Shape of initial conditions
-    #
-    # #Get avaliable GPU's on a rank
-    # gpu_ids = GPUtil.getAvailable(order = 'load',excludeID=[1],limit=10)
-    # has_gpu = 0
-    #
-    # if gpu_ids:
-    #     has_gpu = len(gpu_ids)
-    # has_gpu = comm.gather(has_gpu,root=0)
-    # #-------------------------Determining How Much Work----------------------#
-    #
+    #---------------Data Input setup -------------------------
+    plane_shape = np.shape(y0)  #Shape of initial conditions
+
+
+    #------------------Dividing work based on the architecture-----------------
+    (gpu_sum,cpu_sum) = arch_query()    #This gets architecture information from ranks
+    if rank == master_rank:
+        print(cpu_sum,gpu_sum)
+        # arch_data_split(grid,block_size,num_cpu,num_gpu)
+
     # gpu_blocks,cpu_blocks,gpu_affinity = arch_work_blocks(plane_shape,block_size,gpu_affinity)
     # GPUtil.showUtilization()
     # dev_attrs = dev.getDeviceAttrs(0)
@@ -92,6 +89,39 @@ def sweep(y0, dy, t0, t_b, dt,block_size,ops,devices,gpu_affinity):
     #-------------------------END CUDA TESTING---------------------------#
 
 
+
+
+def arch_data_split(part_dev_id,grid,block_size,num_cpu,num_gpu):
+    """Use this function to split the array based on architecture."""
+    try:
+        #Axes
+        x_ax = 1
+        y_ax = 2
+        #Getting the partition device
+        curr_dev = cuda.Device(part_dev_id)
+        #Checking to max sure block size is not greater than allowable hardward dimensions
+        b1 = (block_size[x_ax]>curr_dev.get_attribute(cuda.device_attribute.MAX_BLOCK_DIM_X))
+        b2 = (block_size[y_ax]>curr_dev.get_attribute(cuda.device_attribute.MAX_BLOCK_DIM_Y))
+        if b1 or b2:
+            raise Exception("Blocksize exceeds hardware specifications.")
+        #Creating work element partition integers
+        par_x = int(grid_shape[x_ax]/block_size[x_ax])
+        par_y = int(grid_shape[y_ax]/block_size[y_ax])
+        # print("Par(x,y,t): ",par_x,", ",par_y,", ",par_t) #Printing partitions
+
+        #Creating times list which includes elements at a specific time level
+
+            # count = 0
+        # for x in np.array_split(t_part,par_x,axis=x_ax):
+        #     for y in np.array_split(x,par_y,axis=y_ax):
+        #         time_level[count]=y
+        #         count+=1
+        # times.append(time_level)
+        # return times
+    except Exception as e:
+        print(e)
+
+
 def arch_work_blocks(plane_shape,block_size,gpu_affinity):
     """Use to determine the closest work split based on the specified gpu_affinity."""
     blocks_x = int(plane_shape[0]/block_size[0])
@@ -101,41 +131,40 @@ def arch_work_blocks(plane_shape,block_size,gpu_affinity):
     cpu_blocks = total_blocks-gpu_blocks
     return (gpu_blocks,cpu_blocks,gpu_blocks/cpu_blocks)
 
-def arch_2D_split(part_dev_id,grid,block_size):
-    """Use this function to split the array based on architecture."""
-    try:
-        #Axes
-        t_ax = 0
-        x_ax = 1
-        y_ax = 2
-        #Getting the partition device
-        curr_dev = cuda.Device(part_dev_id)
-        #Checking to max sure block size is not greater than allowable hardward dimensions
-        b1 = (block_size[x_ax]>curr_dev.get_attribute(cuda.device_attribute.MAX_BLOCK_DIM_X))
-        b2 = (block_size[y_ax]>curr_dev.get_attribute(cuda.device_attribute.MAX_BLOCK_DIM_Y))
-        b3 = (block_size[t_ax]>curr_dev.get_attribute(cuda.device_attribute.MAX_BLOCK_DIM_Z))
-        if b1 or b2 or b3:
-            raise Exception("Blocksize exceeds hardware specifications.")
-        #Creating work element partition integers
-        par_t = int(grid_shape[t_ax]/block_size[t_ax])
-        par_x = int(grid_shape[x_ax]/block_size[x_ax])
-        par_y = int(grid_shape[y_ax]/block_size[y_ax])
-        # print("Par(x,y,t): ",par_x,", ",par_y,", ",par_t) #Printing partitions
+### ----------------------------- COMPLETED FUNCTIONS -----------------------###
+def arch_query():
+    """Use this method to query the system for information about its hardware"""
+    #-----------------------------MPI setup--------------------------------
+    comm = MPI.COMM_WORLD
+    master_rank = 0 #master rank
+    num_ranks = comm.Get_size() #number of ranks
+    rank = comm.Get_rank()  #current rank
+    rank_info = None    #Set to none for later gather
+    cores = mp.cpu_count()  #getting cores of each rank with multiprocessing package
 
-        #Creating times list which includes elements at a specific time level
-        time_parts = np.array_split(grid,par_t,axis=t_ax)
-        times = list()
-        for t_part in time_parts:
-            time_level = dict()
-            count = 0
-            for x in np.array_split(t_part,par_x,axis=x_ax):
-                for y in np.array_split(x,par_y,axis=y_ax):
-                    time_level[count]=y
-                    count+=1
-            times.append(time_level)
-        return times
-    except Exception as e:
-        print(e)
+                            #REMOVE ME AFTER TESTING
+    #####################################################################
+    #Subtracting cores for those used by virtual cluster.
+    if rank == master_rank:
+        cores -= 8
+    #####################################################################
+
+    #Getting avaliable GPUs with GPUtil
+    gpu_ids = GPUtil.getAvailable(order = 'load',excludeID=[1],limit=10)
+    gpus = 0
+    if gpu_ids:
+        gpus = len(gpu_ids)
+    #Gathering all of the information to the master rank
+    rank_info = comm.gather((rank, cores, gpus),root=0)
+    if rank == master_rank:
+        gpu_sum = 0
+        cpu_sum = 0
+        #Getting total number of cpu's and gpu
+        for ri in rank_info:
+            cpu_sum += ri[1]
+            gpu_sum += ri[2]
+        return gpu_sum, cpu_sum
+    return 0,0
 
 def source_code_read(filename):
     """Use this function to generate a multi-line string for pycuda from a source file."""
@@ -150,6 +179,7 @@ def source_code_read(filename):
 
 
 if __name__ == "__main__":
+    # print("Starting execution.")
     dims = (5*256,5*256,4)
     y0 = np.zeros(dims)+1
     dy = [0.1,0.1]
---
+++ vc_push_pysweep.py
@@ -0,0 +1,4 @@
+import os
+# This sends the updated file to the other ranks
+os.system("scp_vc ~/pysweep/pysweep.py ~/pysweep")
+# os.system("sleep 1")
