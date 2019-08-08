9141d47a1c39d009d20cbee426b14a2db2fffb7b
walkanth <walkanth@oregonstate.edu>
2019-08-08 11:11:39 -0700

    Need to do some kernel debugging.

--- src/sweep/sweep.py
+++ src/sweep/sweep.py
@@ -74,7 +74,7 @@ def sweep(arr0,targs,dx,dy,ops,block_size,gpu_source,cpu_source,affinity=1,dType
         #-------------MPI Set up----------------------------#
     comm = MPI.COMM_WORLD
     processor = MPI.Get_processor_name()
-    master_rank = 0 #master rank
+    master_rank = ZERO #master rank
     num_ranks = comm.Get_size() #number of ranks
     rank = comm.Get_rank()  #current rank
     gpu_rank = False    #Says whether or not the rank is a GPU rank
@@ -82,51 +82,51 @@ def sweep(arr0,targs,dx,dy,ops,block_size,gpu_source,cpu_source,affinity=1,dType
     rank_info = comm.allgather((rank,processor,gpu_ids,gpu_rank,None))
     printer = pysweep_printer(rank,master_rank) #This is a printer to print only master rank data
     #Max Swept Steps from block_size and other constants
-    MPSS = int(min(block_size[:-1])/(2*ops+1))+1 #Max Pyramid Swept Step
-    MOSS = int(2*min(block_size[:-1])/(2*ops+1))   #Max Octahedron Swept Step
+    MPSS = int(min(block_size[:-ONE])/(TWO*ops+ONE))+ONE #Max Pyramid Swept Step
+    MOSS = int(TWO*min(block_size[:-ONE])/(TWO*ops+ONE))   #Max Octahedron Swept Step
     printer(MPSS)
     #Splits for shared array
-    SPLITX = int(block_size[0]/2)+2*ops    #Split computation shift - add ops
-    SPLITY = int(block_size[1]/2)+2*ops    #Split computation shift
+    SPLITX = int(block_size[ZERO]/TWO)+TWO*ops    #Split computation shift - add ops
+    SPLITY = int(block_size[ONE]/TWO)+TWO*ops    #Split computation shift
 
     #----------------Data Input setup -------------------------#
-    time_steps = int((targs[1]-targs[0])/targs[2])+1  #Number of time steps +1 becuase initial time step
+    time_steps = int((targs[ONE]-targs[ZERO])/targs[TWO])+ONE  #Number of time steps +ONE becuase initial time step
     itemsize = int(dType.itemsize)
     #Global swept step
-    GST = 0 #This is a variable to track which swept step the simulation is on
-    MGST =   2 if time_steps<MOSS else int(np.ceil(time_steps/MOSS))
+    GST = ZERO #This is a variable to track which swept step the simulation is on
+    MGST =   TWO if time_steps<MOSS else int(np.ceil(time_steps/MOSS))
 
     #Create shared process array for data transfer
-    shared_shape = (MOSS,arr0.shape[0],arr0.shape[1]+SPLITX,arr0.shape[2]+SPLITY)
+    shared_shape = (MOSS,arr0.shape[ZERO],arr0.shape[ONE]+SPLITX,arr0.shape[TWO]+SPLITY)
     shared_arr = create_CPU_sarray(comm,shared_shape,dType,np.prod(shared_shape)*itemsize)
     """PRINTER"""
     printer(shared_shape)
     #Setting initial conditions
     if rank == master_rank:
-        shared_arr[0,:,ops:arr0.shape[1]+ops,ops:arr0.shape[2]+ops] = arr0[:,:,:]
+        shared_arr[ZERO,:,ops:arr0.shape[ONE]+ops,ops:arr0.shape[TWO]+ops] = arr0[:,:,:]
     comm.Barrier()  #Ensure that all initial data has been written to the shared array and sync ranks
 
     #Determine which ranks are GPU ranks and create shared array data
     if rank == master_rank:
         new_rank_info, total_gpus = get_gpu_ranks(rank_info,affinity)
-        if affinity==0:
-            total_gpus = 0
+        if affinity==ZERO:
+            total_gpus = ZERO
         else:
             total_gpus = num_ranks if num_ranks<total_gpus else total_gpus
         total_cpus = num_ranks-total_gpus
-        affinity = affinity if total_cpus != 0 else 1   #sets affinity to 1 if there are no cpus avaliable
+        affinity = affinity if total_cpus != ZERO else ONE   #sets affinity to ONE if there are no cpus avaliable
         #Adjust for number of cpus/gpus
         # affinity*=(total_gpus/total_cpus)
-        # affinity = affinity if affinity <= 1 else 1 #If the number becomes greater than 1 due to adjusting for number of cpus/gpus set to 1
+        # affinity = affinity if affinity <= ONE else ONE #If the number becomes greater than ONE due to adjusting for number of cpus/gpus set to ONE
         # printer(affinity)
-        # total_cpus = total_cpus if total_cpus%2==0 else total_cpus-total_cpus%2 #Making total cpus divisible by 2
+        # total_cpus = total_cpus if total_cpus%TWO==ZERO else total_cpus-total_cpus%TWO #Making total cpus divisible by TWO
         gpu_ranks = list()
         cpu_ranks = list()
         for ri in new_rank_info:
             if ri[3]:
-                gpu_ranks.append(ri[0])
+                gpu_ranks.append(ri[ZERO])
             else:
-                cpu_ranks.append(ri[0])
+                cpu_ranks.append(ri[ZERO])
         #Getting slices for data
         gpu_slices,cpu_slices = affinity_split(affinity,block_size,arr0.shape,total_gpus,printer)
     else:
@@ -151,8 +151,8 @@ def sweep(arr0,targs,dx,dy,ops,block_size,gpu_source,cpu_source,affinity=1,dType
     cpu_slices = comm.bcast(cpu_slices,root=master_rank)
 
     #New master ranks for each architecture
-    gpu_master_rank = gpu_ranks[0] if len(gpu_ranks)>0 else None
-    cpu_master_rank = cpu_ranks[0] if len(cpu_ranks)>0 else None
+    gpu_master_rank = gpu_ranks[ZERO] if len(gpu_ranks)>ZERO else None
+    cpu_master_rank = cpu_ranks[ZERO] if len(cpu_ranks)>ZERO else None
 
     #Creating GPU group comm
     gpu_group = comm.group.Incl(gpu_ranks)
@@ -171,29 +171,29 @@ def sweep(arr0,targs,dx,dy,ops,block_size,gpu_source,cpu_source,affinity=1,dType
     regions = create_write_regions(rank,gargs,cargs,block_size,ops,MOSS,SPLITX,SPLITY,printer)
     regions = create_read_regions(regions,ops)+regions
     #Regions: (read region, shifted read, write, shifted write)
-    local_array = local_array_create(shared_arr,regions[0],dType)
+    local_array = local_array_create(shared_arr,regions[ZERO],dType)
     # Specifying which source mod to use
     if gpu_rank:
         # Creating cuda device and Context
         cuda.init()
-        cuda_device = cuda.Device(new_rank_info[-1])
+        cuda_device = cuda.Device(new_rank_info[-ONE])
         cuda_context = cuda_device.make_context()
         #Creating local GPU array with split
-        grid_size = (int(local_array.shape[2]/block_size[0]),int(local_array.shape[3]/block_size[1]))   #Grid size
+        grid_size = (int(local_array.shape[TWO]/block_size[ZERO]),int(local_array.shape[3]/block_size[ONE]))   #Grid size
         #Creating constants
-        NV = local_array.shape[1]
-        SGIDS = block_size[0]*block_size[1]+2*ops
-        VARS =  SGIDS*grid_size[0]*grid_size[1]
+        NV = local_array.shape[ONE]
+        SGIDS = block_size[ZERO]*block_size[ONE]+TWO*ops
+        VARS =  SGIDS*grid_size[ZERO]*grid_size[ONE]
         TIMES = VARS*NV
         DX = dx
         DY = dy
-        DT = targs[2]
+        DT = targs[TWO]
         const_dict = ({"NV":NV,"DX":DX,"DT":DT,"DY":DY,"SGIDS":SGIDS,"VARS":VARS
                     ,"TIMES":TIMES,"SPLITX":SPLITX,"SPLITY":SPLITY,"MPSS":MPSS,"MOSS":MOSS,"OPS":ops})
         #Building CUDA source code
         source_mod = build_gpu_source(gpu_source)
         constant_copy(source_mod,const_dict,add_consts)
-    elif regions[0]:
+    elif regions[ZERO]:
         source_mod = build_cpu_source(cpu_source) #Building Python source code
         grid_size = None
 
@@ -208,27 +208,27 @@ def sweep(arr0,targs,dx,dy,ops,block_size,gpu_source,cpu_source,affinity=1,dType
 
     #UpPyramid Step
     if LAB:
-        UpPyramid(source_mod,local_array, gpu_rank,block_size,grid_size,regions[2],shared_arr,idx_sets,ops,printer) #THis modifies shared array
+        UpPyramid(source_mod,local_array, gpu_rank,block_size,grid_size,regions[TWO],shared_arr,idx_sets,ops,printer) #THis modifies shared array
     comm.Barrier()
-    # printer(shared_arr[1,0,:,:])
+    # printer(shared_arr[ONE,ZERO,:,:])
     # #Communicate edges
     # if LAB and rank == master_rank:
-    #     edge_comm(shared_arr,SPLITX,SPLITY,GST%2)
+    #     edge_comm(shared_arr,SPLITX,SPLITY,GST%TWO)
     # comm.Barrier()
-    # GST+=1  #Increment global swept step
+    # GST+=ONE  #Increment global swept step
     # Octahedron steps
     # while(GST<MGST):
     #     if LAB:
-    #         cregion = regions[GST%2]
+    #         cregion = regions[GST%TWO]
     #         #Reading local array
     #         local_array = shared_arr[cregion]
     #         #Octahedron Step
     #         Octahedron(source_mod,local_array, gpu_rank,block_size,grid_size,cregion,shared_arr,idx_sets)
     #     comm.Barrier()  #Write barrier
     #     if rank == master_rank:
-    #         edge_comm(shared_arr,SPLITX,SPLITY,GST%2)
+    #         edge_comm(shared_arr,SPLITX,SPLITY,GST%TWO)
     #     comm.Barrier() #Edge transfer barrier
-    #     GST+=1
+    #     GST+=ONE
     #     #Add JSON WRITE HERE and SHIFT
     #
     # #Down Pyramid Step
