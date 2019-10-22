0a7140b081d20adb03799785cb063d217c3b4c05
Anthony Walker <walkanth@oregonstate.edu>
2019-10-22 11:31:09 -0700

    Functions moved to functions file

--- pysweep/sweep/dcore/functions.py
+++ pysweep/sweep/dcore/functions.py
@@ -1,7 +1,12 @@
 #Programmer: Anthony Walker
 #This file contains all of the necessary functions for implementing the swept rule.
+from .dcore import sgs, decomp
+#CUDA Imports
+import pycuda.driver as cuda
+from pycuda.compiler import SourceModule
+import numpy as np
 
-def FirstPrism(sarr,garr,blocks,total_cpu_block,up_sets,x_sets,gts,mpi_pool,pargs):
+def FirstPrism(sarr,garr,blocks,gts,pargs,mpi_pool,total_cpu_block):
     """
     This is the starting pyramid for the 2D heterogeneous swept rule cpu portion.
     arr-the array that will be solved (t,v,x,y)
@@ -12,7 +17,7 @@ def FirstPrism(sarr,garr,blocks,total_cpu_block,up_sets,x_sets,gts,mpi_pool,parg
     #Splitting between cpu and gpu
     if GRB:
         arr_gpu = cuda.mem_alloc(garr.nbytes)
-        garr = copy_s_to_g(sarr,garr,blocks,BS)
+        garr = decomp.copy_s_to_g(sarr,garr,blocks,BS)
         cuda.memcpy_htod(arr_gpu,garr)
         SM.get_function("UpPyramid")(arr_gpu,np.int32(gts),grid=GRD, block=BS,shared=ssb)
         SM.get_function("YBridge")(arr_gpu,np.int32(gts),grid=(GRD[0],GRD[1]-1), block=BS,shared=ssb)
@@ -21,12 +26,12 @@ def FirstPrism(sarr,garr,blocks,total_cpu_block,up_sets,x_sets,gts,mpi_pool,parg
         sarr[blocks]=garr[:,:,:,BS[0]:-BS[0]]
     else:   #CPUs do this
         cblocks,xblocks = zip(*blocks)
-        mpi_pool.map(dCPU_UpPyramid,zip(cblocks,repeat(gts)))
-        mpi_pool.map(dCPU_Ybridge,zip(xblocks,repeat(gts)))
+        mpi_pool.map(dCPU_UpPyramid,cblocks)
+        mpi_pool.map(dCPU_Ybridge,xblocks)
         #Copy result to MPI shared process array
-        sarr[total_cpu_block] = carr[:,:,:,:]
+        sarr[total_cpu_block] = sgs.carr[:,:,:,:]
 
-def UpPrism(sarr,garr,blocks,total_cpu_block,up_sets,x_sets,gts,mpi_pool,pargs):
+def UpPrism(sarr,garr,blocks,up_sets,x_sets,gts,pargs,mpi_pool,total_cpu_block):
     """
     This is the starting pyramid for the 2D heterogeneous swept rule cpu portion.
     arr-the array that will be solved (t,v,x,y)
@@ -37,37 +42,35 @@ def UpPrism(sarr,garr,blocks,total_cpu_block,up_sets,x_sets,gts,mpi_pool,pargs):
     #Splitting between cpu and gpu
     if GRB:
         arr_gpu = cuda.mem_alloc(garr.nbytes)
-        garr = copy_s_to_g(sarr,garr,blocks,BS)
+        garr = decomp.copy_s_to_g(sarr,garr,blocks,BS)
         cuda.memcpy_htod(arr_gpu,garr)
         SM.get_function("UpPyramid")(arr_gpu,np.int32(gts),grid=GRD, block=BS,shared=ssb)
         SM.get_function("YBridge")(arr_gpu,np.int32(gts),grid=(GRD[0],GRD[1]-1), block=BS,shared=ssb)
         cuda.Context.synchronize()
         cuda.memcpy_dtoh(garr,arr_gpu)
-        pm(garr,2,'%.0f')
+        printer.pm(garr,2,'%.0f')
         sarr[blocks]=garr[:,:,:,BS[0]:-BS[0]]
     else:   #CPUs do this
         cblocks,xblocks = zip(*blocks)
-        mpi_pool.map(dCPU_UpPyramid,zip(cblocks,repeat(gts)))
-        mpi_pool.map(dCPU_Ybridge,zip(xblocks,repeat(gts)))
+        mpi_pool.map(dCPU_UpPyramid,cblocks)
+        mpi_pool.map(dCPU_Ybridge,xblocks)
         #Copy result to MPI shared process array
-        sarr[total_cpu_block] = carr[:,:,:,:]
+        sarr[total_cpu_block] = sgs.carr[:,:,:,:]
 
 
 def dCPU_UpPyramid(block):
     """Use this function to build the Up Pyramid."""
     #UpPyramid of Swept Step
-    global carr
-    block,ct = block
-    for ts,swept_set in enumerate(up_sets,start=TSO-1):
+    ct = sgs.gts
+    for ts,swept_set in enumerate(sgs.up_sets,start=sgs.TSO-1):
         #Calculating Step
-        carr[block] = SM.step(carr[block],swept_set,ts,ct)
+        sgs.carr[block] = sgs.SM.step(sgs.carr[block],swept_set,ts,ct)
         ct+=1
 
 def dCPU_Ybridge(block):
     """Use this function to build the XBridge."""
-    global carr,TSO
-    block,ct = block
-    for ts,swept_set in enumerate(x_sets,start=TSO-1):
+    ct = sgs.gts
+    for ts,swept_set in enumerate(sgs.y_sets,start=sgs.TSO-1):
         #Calculating Step
-        carr[block] = SM.step(carr[block],swept_set,ts,ct)
+        sgs.carr[block] = sgs.SM.step(sgs.carr[block],swept_set,ts,ct)
         ct+=1
--- pysweep/sweep/dist_sweep_engine.py
+++ pysweep/sweep/dist_sweep_engine.py
@@ -147,7 +147,7 @@ def dsweep_engine():
     #-------------------------------SWEPT RULE---------------------------------------------#
     pargs = (sgs.SM,GRB,BS,GRD,OPS,TSO,ssb) #Passed arguments to the swept functions
     #-------------------------------FIRST PYRAMID-------------------------------------------#
-    FirstPrism(sarr,garr,blocks,sgs.gts,pargs,mpi_pool,total_cpu_block)
+    functions.FirstPrism(sarr,garr,blocks,sgs.gts,pargs,mpi_pool,total_cpu_block)
     node_comm.Barrier()
     if rank == node_master:
         for i in range(2,4,1):
@@ -163,74 +163,6 @@ def dsweep_engine():
     hdf5_file.close()
 
 
-def FirstPrism(sarr,garr,blocks,gts,pargs,mpi_pool,total_cpu_block):
-    """
-    This is the starting pyramid for the 2D heterogeneous swept rule cpu portion.
-    arr-the array that will be solved (t,v,x,y)
-    fcn - the function that solves the problem in question
-    OPS -  the number of atomic operations
-    """
-    SM,GRB,BS,GRD,OPS,TSO,ssb = pargs
-    #Splitting between cpu and gpu
-    if GRB:
-        arr_gpu = cuda.mem_alloc(garr.nbytes)
-        garr = decomp.copy_s_to_g(sarr,garr,blocks,BS)
-        cuda.memcpy_htod(arr_gpu,garr)
-        SM.get_function("UpPyramid")(arr_gpu,np.int32(gts),grid=GRD, block=BS,shared=ssb)
-        SM.get_function("YBridge")(arr_gpu,np.int32(gts),grid=(GRD[0],GRD[1]-1), block=BS,shared=ssb)
-        cuda.Context.synchronize()
-        cuda.memcpy_dtoh(garr,arr_gpu)
-        sarr[blocks]=garr[:,:,:,BS[0]:-BS[0]]
-    else:   #CPUs do this
-        cblocks,xblocks = zip(*blocks)
-        mpi_pool.map(dCPU_UpPyramid,cblocks)
-        mpi_pool.map(dCPU_Ybridge,xblocks)
-        #Copy result to MPI shared process array
-        sarr[total_cpu_block] = sgs.carr[:,:,:,:]
-
-def UpPrism(sarr,garr,blocks,up_sets,x_sets,gts,pargs,mpi_pool,total_cpu_block):
-    """
-    This is the starting pyramid for the 2D heterogeneous swept rule cpu portion.
-    arr-the array that will be solved (t,v,x,y)
-    fcn - the function that solves the problem in question
-    OPS -  the number of atomic operations
-    """
-    SM,GRB,BS,GRD,OPS,TSO,ssb = pargs
-    #Splitting between cpu and gpu
-    if GRB:
-        arr_gpu = cuda.mem_alloc(garr.nbytes)
-        garr = decomp.copy_s_to_g(sarr,garr,blocks,BS)
-        cuda.memcpy_htod(arr_gpu,garr)
-        SM.get_function("UpPyramid")(arr_gpu,np.int32(gts),grid=GRD, block=BS,shared=ssb)
-        SM.get_function("YBridge")(arr_gpu,np.int32(gts),grid=(GRD[0],GRD[1]-1), block=BS,shared=ssb)
-        cuda.Context.synchronize()
-        cuda.memcpy_dtoh(garr,arr_gpu)
-        printer.pm(garr,2,'%.0f')
-        sarr[blocks]=garr[:,:,:,BS[0]:-BS[0]]
-    else:   #CPUs do this
-        cblocks,xblocks = zip(*blocks)
-        mpi_pool.map(dCPU_UpPyramid,cblocks)
-        mpi_pool.map(dCPU_Ybridge,xblocks)
-        #Copy result to MPI shared process array
-        sarr[total_cpu_block] = sgs.carr[:,:,:,:]
-
-
-def dCPU_UpPyramid(block):
-    """Use this function to build the Up Pyramid."""
-    #UpPyramid of Swept Step
-    ct = sgs.gts
-    for ts,swept_set in enumerate(sgs.up_sets,start=sgs.TSO-1):
-        #Calculating Step
-        sgs.carr[block] = sgs.SM.step(sgs.carr[block],swept_set,ts,ct)
-        ct+=1
-
-def dCPU_Ybridge(block):
-    """Use this function to build the XBridge."""
-    ct = sgs.gts
-    for ts,swept_set in enumerate(sgs.y_sets,start=sgs.TSO-1):
-        #Calculating Step
-        sgs.carr[block] = sgs.SM.step(sgs.carr[block],swept_set,ts,ct)
-        ct+=1
 
 
 #Statement to execute dsweep
