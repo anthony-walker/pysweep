746b22a7fcb3e96be8f5ca054d218214271aa06f
Anthony Walker <asw42695@gmail.com>
2019-10-31 16:45:48 -0700

    CT

--- dtest.py
+++ dtest.py
@@ -4,12 +4,9 @@ import numpy as np
 if __name__ == "__main__":
     # nx = ny = 120
     ny = 12
-    nx = 60
+    nx = 120
     bs = 12
     t0 = 0
-    tf = 0.1
-    dt = 0.01
-    dx = dy = 0.1
     gamma = 1.4
     arr = np.ones((4,nx,ny))
     # ct = 0
@@ -18,14 +15,18 @@ if __name__ == "__main__":
     #         arr[0,i,j] = ct
     #         ct+=1
     # arr[:,256:,:] += 1
-    X = 1
-    Y = 1
-    tso = 2
+    X = 10
+    Y = 10
+    tso = 1
     ops = 2
     aff = 0 #Dimensions and steps
     dx = X/nx
     dy = Y/ny
+    alpha = 5
+    Fo = 0.24
+    dt = Fo*(X/nx)**2/alpha
+    tf = dt*100
     #Changing arguments
-    gargs = (t0,tf,dt,dx,dy,gamma)
-    swargs = (tso,ops,bs,aff,"./pysweep/equations/eqt.h","./pysweep/equations/eqt.py")
-    dsweep(arr,gargs,swargs,filename="test",exid=[1])
+    gargs = (t0,tf,dt,dx,dy,alpha)
+    swargs = (tso,ops,bs,aff,"./pysweep/equations/hde.h","./pysweep/equations/hde.py")
+    dsweep(arr,gargs,swargs,filename="test",exid=[])
