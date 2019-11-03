5e137843838347a191774b529d538642c6888ff6
Anthony Walker <asw42695@gmail.com>
2019-11-03 12:03:35 -0800

    CT

--- pysweep/sweep/dse.py
+++ pysweep/sweep/dse.py
@@ -112,65 +112,67 @@ def dsweep_engine():
     node_ranks = node_comm.bcast(node_ranks)
     node_info = node_comm.bcast(node_info)
     #----------------------__Removing Unwanted MPI Processes------------------------#
+    print(rank,ranks_to_remove)
     node_comm,comm = dcore.mpi_destruction(rank,node_ranks,comm,ranks_to_remove,all_ranks)
+    print(rank,ranks_to_remove)
     gpu_rank,blocks = decomp.nsplit(rank,node_master,node_comm,num_gpus,node_info,BS,arr0.shape,gpu_rank)
     #Checking to ensure that there are enough
-    assert total_num_gpus >= node_comm.Get_size() if AF == 1 else True,"Not enough GPUs for ranks"
-    #---------------------------Creating and Filling Shared Array-------------#
-    shared_shape = (MOSS+TSO+ONE,arr0.shape[0],int(sum(node_info[2:])*BS[0]),arr0.shape[2])
-    sarr = decomp.create_CPU_sarray(node_comm,shared_shape,dType,np.zeros(shared_shape).nbytes)
-    ssb = np.zeros((2,arr0.shape[ZERO],BS[0]+2*OPS,BS[1]+2*OPS),dtype=dType).nbytes
-    #Filling shared array
-    if NMB:
-        gsc = (slice(0,arr0.shape[1],1),slice(int(node_info[0]*BS[0]),int(node_info[1]*BS[0]),1),slice(0,arr0.shape[2],1))
-        sarr[TSO-ONE,:,:,:] =  arr0[gsc]
-    else:
-        gsc = None
-    #Making blocks match array other dimensions
-    bsls = [slice(0,i,1) for i in shared_shape]
-    blocks = (bsls[0],bsls[1],blocks,bsls[3])
-    GRB = True if gpu_rank is not None else False
-    # ------------------- Operations specifically for GPus and CPUs------------------------#
-    if GRB:
-        # Creating cuda device and context
-        cuda.init()
-        cuda_device = cuda.Device(gpu_rank)
-        cuda_context = cuda_device.make_context()
-        block_shape,GRD,garr = dcore.gpu_core(blocks,BS,OPS,GS,CS,gargs,GRB,MPSS,MOSS,TSO)
-        mpi_pool,carr,up_sets,down_sets,oct_sets,x_sets,y_sets,total_cpu_block = None,None,None,None,None,None,None,None
-    else:
-        GRD,block_shape,garr = None,None,None
-        blocks,total_cpu_block = dcore.cpu_core(sarr,blocks,shared_shape,OPS,BS,CS,GRB,gargs,MPSS)
-        mpi_pool = mp.Pool(os.cpu_count()-node_comm.Get_size()+1)
-    # ------------------------------HDF5 File------------------------------------------#
-    hdf5_file, hdf5_data = dcore.make_hdf5(filename,cluster_master,comm,rank,BS,arr0,time_steps,AF,dType)
-    comm.Barrier() #Ensure all processes are prepared to solve
-    # -------------------------------SWEPT RULE---------------------------------------------#
-    pargs = (sgs.SM,GRB,BS,GRD,OPS,TSO,ssb) #Passed arguments to the swept functions
-    node_comm.Barrier()
-    # -------------------------------FIRST PRISM AND COMMUNICATION-------------------------------------------#
-    functions.FirstPrism(sarr,garr,blocks,sgs.gts,pargs,mpi_pool,total_cpu_block)
-    node_comm.Barrier()
-    functions.first_forward(NMB,GRB,node_comm,cluster_comm,comranks,sarr,SPLITX,total_cpu_block)
-    cwt = 1
-    # -------------------------------SWEPT LOOP--------------------------------------------#
-    step = cycle([functions.send_backward,functions.send_forward])
-    for i in range(MGST):
-        functions.UpPrism(sarr,garr,blocks,sgs.gts,pargs,mpi_pool,total_cpu_block)
-        node_comm.Barrier()
-        cwt = next(step)(cwt,sarr,hdf5_data,gsc,NMB,GRB,node_comm,cluster_comm,comranks,SPLITX,total_cpu_block)
-    #Do LastPrism Here then Write all of the remaining data
-    functions.LastPrism(sarr,garr,blocks,sgs.gts,pargs,mpi_pool,total_cpu_block)
-    node_comm.Barrier()
-    next(step)(cwt,sarr,hdf5_data,gsc,NMB,GRB,node_comm,cluster_comm,comranks,SPLITX,total_cpu_block)
-    # Clean Up - Pop Cuda Contexts and Close Pool
-    if GRB:
-        cuda_context.pop()
-    comm.Barrier()
-    hdf5_file.close()
-    #Removing input file.
-    if NMB:
-        os.system("rm "+"input_file.hdf5")
+    # assert total_num_gpus >= node_comm.Get_size() if AF == 1 else True,"Not enough GPUs for ranks"
+    # #---------------------------Creating and Filling Shared Array-------------#
+    # shared_shape = (MOSS+TSO+ONE,arr0.shape[0],int(sum(node_info[2:])*BS[0]),arr0.shape[2])
+    # sarr = decomp.create_CPU_sarray(node_comm,shared_shape,dType,np.zeros(shared_shape).nbytes)
+    # ssb = np.zeros((2,arr0.shape[ZERO],BS[0]+2*OPS,BS[1]+2*OPS),dtype=dType).nbytes
+    # #Filling shared array
+    # if NMB:
+    #     gsc = (slice(0,arr0.shape[1],1),slice(int(node_info[0]*BS[0]),int(node_info[1]*BS[0]),1),slice(0,arr0.shape[2],1))
+    #     sarr[TSO-ONE,:,:,:] =  arr0[gsc]
+    # else:
+    #     gsc = None
+    # #Making blocks match array other dimensions
+    # bsls = [slice(0,i,1) for i in shared_shape]
+    # blocks = (bsls[0],bsls[1],blocks,bsls[3])
+    # GRB = True if gpu_rank is not None else False
+    # # ------------------- Operations specifically for GPus and CPUs------------------------#
+    # if GRB:
+    #     # Creating cuda device and context
+    #     cuda.init()
+    #     cuda_device = cuda.Device(gpu_rank)
+    #     cuda_context = cuda_device.make_context()
+    #     block_shape,GRD,garr = dcore.gpu_core(blocks,BS,OPS,GS,CS,gargs,GRB,MPSS,MOSS,TSO)
+    #     mpi_pool,carr,up_sets,down_sets,oct_sets,x_sets,y_sets,total_cpu_block = None,None,None,None,None,None,None,None
+    # else:
+    #     GRD,block_shape,garr = None,None,None
+    #     blocks,total_cpu_block = dcore.cpu_core(sarr,blocks,shared_shape,OPS,BS,CS,GRB,gargs,MPSS)
+    #     mpi_pool = mp.Pool(os.cpu_count()-node_comm.Get_size()+1)
+    # # ------------------------------HDF5 File------------------------------------------#
+    # hdf5_file, hdf5_data = dcore.make_hdf5(filename,cluster_master,comm,rank,BS,arr0,time_steps,AF,dType)
+    # comm.Barrier() #Ensure all processes are prepared to solve
+    # # -------------------------------SWEPT RULE---------------------------------------------#
+    # pargs = (sgs.SM,GRB,BS,GRD,OPS,TSO,ssb) #Passed arguments to the swept functions
+    # node_comm.Barrier()
+    # # -------------------------------FIRST PRISM AND COMMUNICATION-------------------------------------------#
+    # functions.FirstPrism(sarr,garr,blocks,sgs.gts,pargs,mpi_pool,total_cpu_block)
+    # node_comm.Barrier()
+    # functions.first_forward(NMB,GRB,node_comm,cluster_comm,comranks,sarr,SPLITX,total_cpu_block)
+    # cwt = 1
+    # # -------------------------------SWEPT LOOP--------------------------------------------#
+    # step = cycle([functions.send_backward,functions.send_forward])
+    # for i in range(MGST):
+    #     functions.UpPrism(sarr,garr,blocks,sgs.gts,pargs,mpi_pool,total_cpu_block)
+    #     node_comm.Barrier()
+    #     cwt = next(step)(cwt,sarr,hdf5_data,gsc,NMB,GRB,node_comm,cluster_comm,comranks,SPLITX,total_cpu_block)
+    # #Do LastPrism Here then Write all of the remaining data
+    # functions.LastPrism(sarr,garr,blocks,sgs.gts,pargs,mpi_pool,total_cpu_block)
+    # node_comm.Barrier()
+    # next(step)(cwt,sarr,hdf5_data,gsc,NMB,GRB,node_comm,cluster_comm,comranks,SPLITX,total_cpu_block)
+    # # Clean Up - Pop Cuda Contexts and Close Pool
+    # if GRB:
+    #     cuda_context.pop()
+    # comm.Barrier()
+    # hdf5_file.close()
+    # #Removing input file.
+    # if NMB:
+    #     os.system("rm "+"input_file.hdf5")
 #Statement to execute dsweep
 dsweep_engine()
 #Statement to finalize MPI processes
