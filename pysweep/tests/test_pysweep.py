fc47f96682386299abce6cea07e105087860770f
asw42695 <walkanth@oregonstate.edu>
2019-11-03 14:14:46 -0800

    Execution will fail if more hosts are avaliable than number of processes specified, this can be avoided by specifying a host file but adding logic to handle this is the goal

--- pysweep/tests/test_pysweep.py
+++ pysweep/tests/test_pysweep.py
@@ -146,7 +146,7 @@ def test_dsweep_hde(args=(8, 120, 0.5, 10, 0.24, 5, 12, 3)):
     pts = " -nx "+str(npx)+ " -ny "+str(npx)+" -X "+str(X)+ " -Y "+str(Y)
     if not os.path.isfile(sfp):
         #Create data using solver
-        estr = "mpiexec -n "+str(nps)+"  python ./pysweep/pst.py DSHDE "
+        estr = "mpiexec -n "+str(nps)+" --hostfile=sub-nodes  python ./pysweep/pst.py DSHDE "
         estr += "-b "+str(blks)+" -o 1 --tso 2 -a "+str(aff)+" -g \"./pysweep/equations/hde.h\" -c \"./pysweep/equations/hde.py\" "
         estr += "--hdf5 " + swept_file + pts +time_str + "--alpha "+str(alpha)+" -TH 373 -TL 298"
         os.system(estr)
