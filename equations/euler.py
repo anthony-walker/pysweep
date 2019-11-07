None of the hde's are matching, there was a mix up in file reading, need to determine which is correct. Chased down lead with euler equations, it was numerical precision in python, need to set up more extensive test case.

equations/euler.py
@@ -9,10 +9,10 @@ try:
 except Exception as e:
     pass
 #----------------------------------Globals-------------------------------------#
-gamma = 0
-dtdx = 0
-dtdy = 0
-
+gamma = 1.4
+dtdx = 0.0
+dtdy = 0.0
+gM1 = 0.4
 #----------------------------------End Globals-------------------------------------#
 
 def step(state,iidx,ts,gts):
@@ -22,6 +22,7 @@ def step(state,iidx,ts,gts):
     ts - the current time step
     gts - a step counter that allows implementation of the scheme
     """
+
     half = 0.5
     vs = slice(0,state.shape[1],1)
     for idx,idy in iidx:
@@ -49,6 +50,8 @@ def set_globals(gpu,source_mod,*args):
         dtdy = dt/dy
         global gamma
         gamma = gam
+        global gM1
+        gM1 = gam-1.0
 
 #---------------------------------------------Solving functions
 
@@ -57,8 +60,9 @@ def dfdxy(state,idx):
     #Five point finite volume method
     #Creating indices from given point (idx)
     ops = 2 #number of atomic operations
-    idxx=(idx[0],idx[1],slice(idx[2]-ops,idx[2]+ops+1,1),idx[3])
-    idxy=(idx[0],idx[1],idx[2],slice(idx[3]-ops,idx[3]+ops+1,1))
+    i1,i2,i3,i4 = idx
+    idxx=(i1,i2,slice(i3-ops,i3+ops+1,1),i4)
+    idxy=(i1,i2,i3,slice(i4-ops,i4+ops+1,1))
     idxyt=(idx[0],idx[1],idx[2],slice(idx[3]+ops+1,idx[3]-ops,-1))
     half = 0.5
     #Finding pressure ratio
@@ -66,8 +70,7 @@ def dfdxy(state,idx):
     Pry = pressure_ratio(state[idxy])
     #Finding spatial derivatives
     dfdx = half*direction_flux(state[idxx],Prx,True)
-    # dfdy = half*direction_flux(state[idxy],Pry,False)
-    dfdy = np.zeros(dfdx.shape)
+    dfdy = half*direction_flux(state[idxy],Pry,False)
     return dfdx, dfdy
 
 def pressure_ratio(state):
@@ -92,12 +95,9 @@ def pressure(q):
     q[1] = rho*u
     q[2] = rho*v
     q[3] = rho*e
-    P = (GAMMA-1)*(rho*e-rho/2*(rho*u^2+rho*v^2))
+    P = (GAMMA-1)*(rho*e-(1/2)*(rho*u^2+rho*v^2))
     """
-    HALF = 0.5
-    rho_1_inv = 1/q[0]
-    vss = (rho_1_inv*q[1]*q[1])+(rho_1_inv*q[2]*q[2])
-    return (gamma - 1)*(q[3]-HALF*vss)
+    return gM1*(q[3]-(q[1]*q[1]/(2*q[0]))+(q[2]*q[2]/(2*q[0])))
 
 def direction_flux(state,Pr,xy):
     """Use this method to determine the flux in a particular direction."""
@@ -105,25 +105,16 @@ def direction_flux(state,Pr,xy):
     idx = 2     #This is the index of the point in state (stencil data)
     #Initializing Flux
     flux = np.zeros(len(state[:,idx]))
-    print(state,xy)
     #Atomic Operation 1
     tsl = flimiter(state[:,idx-1],state[:,idx],Pr[idx-2])
     tsr = flimiter(state[:,idx],state[:,idx-1],ONE/Pr[idx-1])
-    # print(tsl,tsr)
     flux += eflux(tsl,tsr,xy)
-    # print(flux)
     flux += espectral(tsl,tsr,xy)
-    # print(flux)
     #Atomic Operation 2
     tsl = flimiter(state[:,idx],state[:,idx+1],Pr[idx-1])
     tsr = flimiter(state[:,idx+1],state[:,idx],ONE/Pr[idx])
-    # print(tsl,tsr)
-    # print(flux)
     flux -= eflux(tsl,tsr,xy)
-    # print(flux)
     flux -= espectral(tsl,tsr,xy)
-    # print(flux)
-
     return flux
 
 def flimiter(qL,qR,Pr):

pst.py
@@ -81,6 +81,35 @@ def StandardHDE(args):
     else:
         decomp.decomp(arr0,gargs,swargs,filename=args.hdf5)
 
+
+def SweptSimple(args):
+    #Analytical properties
+    arr0 = arr = np.ones((4,args.nx,args.ny))
+    #Dimensions and steps
+    dx = args.X/args.nx
+    dy = args.Y/args.ny
+    #Changing arguments
+    gargs = (args.t0,args.tf,args.dt,dx,dy,args.gamma)
+    swargs = (2,2,args.block,args.affinity,os.path.join(epath,'eqt2.h'),os.path.join(epath,'eqt2.py'))
+    if args.distributed:
+        distsweep.dsweep(arr0,gargs,swargs,filename=args.hdf5)
+    else:
+        nodesweep.nsweep(arr0,gargs,swargs,filename=args.hdf5)
+
+def StandardSimple(args):
+    #Analytical properties
+    arr0 = arr = np.ones((4,args.nx,args.ny))
+    #Dimensions and steps
+    dx = args.X/args.nx
+    dy = args.Y/args.ny
+    #Changing arguments
+    gargs = (args.t0,args.tf,args.dt,dx,dy,args.gamma)
+    swargs = (2,2,args.block,args.affinity,os.path.join(epath,'eqt2.h'),os.path.join(epath,'eqt2.py'))
+    if args.distributed:
+        ddecomp.ddecomp(arr0,gargs,swargs,filename=args.hdf5)
+    else:
+        decomp.decomp(arr0,gargs,swargs,filename=args.hdf5)
+
 def str2bool(v):
     if isinstance(v, bool):
        return v
@@ -95,7 +124,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     fmap = {'swept_vortex' : SweptVortex,
                     'standard_vortex' : StandardVortex,"analytical_vortex":AnalyticalVortex,'swept_hde' : SweptHDE,
-                                    'standard_hde' : StandardHDE}
+                                    'standard_hde' : StandardHDE,'swept_simple':SweptSimple,'standard_simple':StandardSimple}
     parser.add_argument('function', choices=fmap.keys())
     parser.add_argument("-b","--block",nargs="?",default=8,type=int)
     parser.add_argument("-a","--affinity",nargs="?",default=0.5,type=float)

tests/test_equations.py
@@ -9,59 +9,37 @@ sys.path.insert(0, path[:-5])
 import distributed.sweep.ccore.source as source
 epath = os.path.join(path[:-5],'equations')
 
-def test_flux():
-    """Use this function to test the python version of the euler code.
-    This test uses a formerly validate 1D code and computes the fluxes in each direction
-    to test whether the 2D fluxes are correctly computing
-    """
+def test_steps():
+    """Use this function to test several steps for 1 point"""
     #Sod Shock BC's
     t0 = 0
-    tf = 1
+    tf = .1
     dt = 0.01
     dx = 0.1
     dy = 0.1
     gamma = 1.4
-    leftBC = (1000,0,0,2500)
+    leftBC = (1,0,0,2.5)
     rightBC = (0.125,0,0,.25)
-    test_shape = (2,4,5,5)
-    num_test = np.zeros((2,4,5,5))
-    for i in range(3):
-        for j in range(3):
-            num_test[0,:,i,j]=leftBC[:]
-    for i in range(2,5):
-        for j in range(2,5):
-            num_test[0,:,i,j]=rightBC[:]
-    xtest = np.zeros(test_shape)
-    ytest = np.zeros(test_shape)
-    for i in range(2):
-        xtest[0,:,i,2]=leftBC
-        xtest[0,:,i+1,2]=rightBC
-        xtest[0,:,i+3,2]=rightBC
-        ytest[0,:,2,i]=leftBC
-        ytest[0,:,2,i+1]=rightBC
-        ytest[0,:,2,i+3]=rightBC
-    Qx = np.zeros((5,3))
-    Qx[:,0] = num_test[0,0,:,2]
-    Qx[:,1] = num_test[0,1,:,2]
-    Qx[:,2] = num_test[0,3,:,2]
-    P = np.zeros(5)
-    P[:] = [1000,1000,0.1,0.1,0.1]
-    #Get source module
+    nx = ny = 10
+    nxh = int(ny/2)
+    nv = 4
+    ops = 2
+    #2D array
+    test_shape = (int((tf-t0)/dt),nv,nx,ny)
+    arr2D = np.zeros(test_shape)
+    for i in range(nx):
+        for j in range(nxh):
+            arr2D[0,:,i,j] = leftBC
+            arr2D[0,:,i,j+nxh] = rightBC
+
+    print(arr2D[0])
+    #Source Modules
     source_mod_2D = source.build_cpu_source(os.path.join(epath,'euler.py'))
     source_mod_2D.set_globals(False,None,*(t0,tf,dt,dx,dy,gamma))
     source_mod_1D = source.build_cpu_source(os.path.join(epath,'euler1D.py'))
-    iidx = (0,slice(0,4,1),2,2)
-    #Testing Flux
-    # print(num_test)
-    dfdx,dn = source_mod_2D.dfdxy(xtest,iidx)
-    # dn,dfdy = source_mod_2D.dfdxy(ytest,iidx)
-    # dfdx = np.delete(dfdx,2)
-    # dfdy = np.delete(dfdy,1)
-    # df = source_mod_1D.fv5p(Qx,P)[2]
-    # assert np.isclose(df.all(),dfdx.all())
-    # assert np.isclose(df.all(),dfdy.all())
-    # print(df)
-    # print(dfdy)
-    # print(dfdx)
+
+
+
+
 if __name__ == "__main__":
-    test_flux()
+    test_steps()

tests/tests.py
@@ -6,9 +6,14 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gsc
 from matplotlib import cm
 import matplotlib.animation as animation
-
 fp = os.path.abspath(__file__)
 path = os.path.dirname(fp)
+sys.path.insert(0, path[:-5])
+import distributed.sweep.ccore.source as source
+import distributed.sweep.ccore.printer as printer
+epath = os.path.join(path[:-5],'equations')
+import warnings
+warnings.simplefilter('ignore')
 
 def test_distributed_decomp_vortex(args=(0.5,0.01,48,0.5,10,12,1),remove_file=True):
     decomp_file = "\""+os.path.join(path,"data/dist_decomp_vortex")+"\""
@@ -154,7 +159,7 @@ def test_swept_hde(args=(1, 48, 0.5, 10, 0.24, 5, 12, 4),remove_file=True):
 def test_comparison_hde(args=(1, 48, 0.5, 10, 0.24, 5, 12, 1),remove_file=True,generate_fig=False):
     """Use this function to compare the values obtain during a run of both solvers"""
     sfn = os.path.join(path,"data/dist_swept_hde")
-    dfn = os.path.join(path,"data/dist_swept_hde")
+    dfn = os.path.join(path,"data/dist_ndecomp_hde")
     swept_file = "\""+sfn+"\""
     decomp_file = "\""+dfn+"\""
     ssfp = "\""+sfn+".hdf5"+"\""
@@ -168,24 +173,32 @@ def test_comparison_hde(args=(1, 48, 0.5, 10, 0.24, 5, 12, 1),remove_file=True,g
     pts = " -nx "+str(npx)+ " -ny "+str(npx)+" -X "+str(X)+ " -Y "+str(Y)
 
     #Create data using solver
-    estr = "mpiexec -n "+str(nps)+" python "+os.path.join(path[:-5],"pst.py")+" standard_hde "
+    estr = "mpiexec -n "+str(4)+" python "+os.path.join(path[:-5],"pst.py")+" standard_hde --distributed \'false\' "
     estr += "-b "+str(blks)+" -a "+str(aff)+" "
     estr += "--hdf5 " + decomp_file + pts +time_str + "--alpha "+str(alpha)+" -TH 373 -TL 298"
     os.system(estr)
 
     #Create data using solver
-    estr = "mpiexec -n "+str(nps)+" python "+os.path.join(path[:-5],"pst.py")+" swept_hde "
+    estr = "mpiexec -n "+str(1)+" python "+os.path.join(path[:-5],"pst.py")+" swept_hde "
     estr += "-b "+str(blks)+" -a "+str(aff)+" "
     estr += "--hdf5 " + swept_file + pts +time_str + "--alpha "+str(alpha)+" -TH 373 -TL 298"
     os.system(estr)
-
     #Opening the data files
+
     swept_hdf5 = h5py.File(sfn+".hdf5", 'r')
-    data = swept_hdf5['data'][:,0,:,:]
-    time = np.arange(0,tf,dt)[:len(data)]
+    decomp_hdf5 = h5py.File(dfn+".hdf5", 'r')
+    tsdata = swept_hdf5['data'][:,0,:,:]
+    tddata = decomp_hdf5['data'][:,0,:,:]
+    absa = abs(tsdata-tddata)
+    max_error = np.amax(absa)
+    idx = np.where(absa==max_error)
+    for j,i in enumerate(absa):
+        print(sum(i)/(npx*npy))
+    assert np.allclose(tsdata[:-10],tddata[:-10])
+
     #Closing files
     swept_hdf5.close()
-
+    decomp_hdf5.close()
     #Generating figure
     if generate_fig:
         comp_gif(dfn+".hdf5",sfn+".hdf5",filename="./hde_comp.gif")
@@ -195,8 +208,161 @@ def test_comparison_hde(args=(1, 48, 0.5, 10, 0.24, 5, 12, 1),remove_file=True,g
         os.system("rm "+ssfp)
         os.system("rm "+sfp)
 
+
+def test_comparison_vortex(args=(2,0.01,48,0.5,10,12,1),remove_file=True,generate_fig=False):
+    """Use this function to compare the values obtain during a run of both solvers"""
+    sfn = os.path.join(path,"data/dist_swept_vtx")
+    dfn = os.path.join(path,"data/dist_decomp_vtx")
+    swept_file = "\""+sfn+"\""
+    decomp_file = "\""+dfn+"\""
+    ssfp = "\""+sfn+".hdf5"+"\""
+    sfp = "\""+dfn+".hdf5"+"\""
+    tf,dt,npx,aff,X,blks,nps = args
+    npy=npx
+    Y=X
+    time_str = " -dt "+str(dt)+" -tf "+str(tf)+ " "
+    pts = " -nx "+str(npx)+ " -ny "+str(npx)+" -X "+str(X)+ " -Y "+str(Y)
+
+    #Create data using solver
+    estr = "mpiexec -n "+str(nps)+" python "+os.path.join(path[:-5],"pst.py")+" standard_vortex "
+    estr += "-b "+str(blks)+" -a "+str(aff)+" "
+    estr += "--hdf5 " + decomp_file + pts +time_str + "--gamma "+str(1.4)
+    os.system(estr)
+
+    #Create data using solver
+    estr = "mpiexec -n "+str(nps)+" python "+os.path.join(path[:-5],"pst.py")+" swept_vortex "
+    estr += "-b "+str(blks)+" -a "+str(aff)+" "
+    estr += "--hdf5 " + swept_file + pts +time_str + "--gamma "+str(1.4)
+    os.system(estr)
+
+    #Opening the data files
+
+    swept_hdf5 = h5py.File(sfn+".hdf5", 'r')
+    decomp_hdf5 = h5py.File(dfn+".hdf5", 'r')
+    tsdata = swept_hdf5['data'][:,0,:,:][:-1]
+    tddata = decomp_hdf5['data'][:,0,:,:]
+    max_error = np.amax(abs(tsdata-tddata))
+
+    # assert np.allclose(tsdata,tddata)
+
+    #Closing files
+    swept_hdf5.close()
+    decomp_hdf5.close()
+    #Generating figure
+    if generate_fig:
+        comp_gif(dfn+".hdf5",sfn+".hdf5",filename="./vtx_comp.gif")
+    #Removing files
+    if remove_file:
+        os.system("rm "+ssfp)
+        os.system("rm "+sfp)
+
+
+def test_comparison_eqt(args=(1, 48, 0.5, 10, 0.24, 5, 12, 1),remove_file=True,generate_fig=False):
+    """Use this function to compare the values obtain during a run of both solvers"""
+    sfn = os.path.join(path,"data/dist_swept_simple")
+    dfn = os.path.join(path,"data/dist_decomp_simple")
+    swept_file = "\""+sfn+"\""
+    decomp_file = "\""+dfn+"\""
+    ssfp = "\""+sfn+".hdf5"+"\""
+    sfp = "\""+dfn+".hdf5"+"\""
+    tf,npx,aff,X,Fo,alpha,blks,nps = args
+    npy=npx
+    Y=X
+    dt = Fo*(X/npx)**2/alpha
+    tf = 200*dt
+    time_str = " -dt "+str(dt)+" -tf "+str(tf)+ " "
+    pts = " -nx "+str(npx)+ " -ny "+str(npx)+" -X "+str(X)+ " -Y "+str(Y)
+
+    #Create data using solver
+    estr = "mpiexec -n "+str(nps)+" python "+os.path.join(path[:-5],"pst.py")+" standard_simple "
+    estr += "-b "+str(blks)+" -a "+str(aff)+" --gamma 1.4 "
+    estr += "--hdf5 " + decomp_file + pts +time_str
+    os.system(estr)
+
+    #Create data using solver
+    estr = "mpiexec -n "+str(nps)+" python "+os.path.join(path[:-5],"pst.py")+" swept_simple "
+    estr += "-b "+str(blks)+" -a "+str(aff)+" --gamma 1.4 "
+    estr += "--hdf5 " + swept_file + pts +time_str
+    os.system(estr)
+
+    #Opening the data files
+
+    swept_hdf5 = h5py.File(sfn+".hdf5", 'r')
+    decomp_hdf5 = h5py.File(dfn+".hdf5", 'r')
+    tsdata = swept_hdf5['data'][:,0,:,:][:-1]
+    tddata = decomp_hdf5['data'][:,0,:,:]
+    assert np.allclose(tsdata,tddata)
+
+    #Closing files
+    swept_hdf5.close()
+    decomp_hdf5.close()
+    #Generating figure
+    if generate_fig:
+        comp_gif(dfn+".hdf5",sfn+".hdf5",filename="./simple_comp.gif")
+
+    #Removing files
+    if remove_file:
+        os.system("rm "+ssfp)
+        os.system("rm "+sfp)
+
+
+def test_flux():
+    """Use this function to test the python version of the euler code.
+    This test uses a formerly validate 1D code and computes the fluxes in each direction
+    to test whether the 2D fluxes are correctly computing
+    """
+    #Sod Shock BC's
+    t0 = 0
+    tf = 1
+    dt = 0.01
+    dx = 0.1
+    dy = 0.1
+    gamma = 1.4
+    leftBC = (1,0,0,2.5)
+    rightBC = (0.125,0,0,.25)
+    test_shape = (2,4,5,5)
+    num_test = np.zeros((2,4,5,5))
+    for i in range(3):
+        for j in range(3):
+            num_test[0,:,i,j]=leftBC[:]
+    for i in range(2,5):
+        for j in range(2,5):
+            num_test[0,:,i,j]=rightBC[:]
+    xtest = np.zeros(test_shape)
+    ytest = np.zeros(test_shape)
+    for i in range(2):
+        xtest[0,:,i,2]=leftBC
+        xtest[0,:,i+1,2]=rightBC
+        xtest[0,:,i+3,2]=rightBC
+        ytest[0,:,2,i]=leftBC
+        ytest[0,:,2,i+1]=rightBC
+        ytest[0,:,2,i+3]=rightBC
+    Qx = np.zeros((5,3))
+    Qx[:,0] = num_test[0,0,:,2]
+    Qx[:,1] = num_test[0,1,:,2]
+    Qx[:,2] = num_test[0,3,:,2]
+    P = np.zeros(5)
+    P[:] = [1,1,0.1,0.1,0.1]
+    #Get source module
+    source_mod_2D = source.build_cpu_source(os.path.join(epath,'euler.py'))
+    source_mod_2D.set_globals(False,None,*(t0,tf,dt,dx,dy,gamma))
+    source_mod_1D = source.build_cpu_source(os.path.join(epath,'euler1D.py'))
+    iidx = (0,slice(0,4,1),2,2)
+
+    #Testing Flux
+    dfdx,dn = source_mod_2D.dfdxy(xtest,iidx)
+    dn,dfdy = source_mod_2D.dfdxy(ytest,iidx)
+    dfdx = np.delete(dfdx,2)
+    dfdy = np.delete(dfdy,1)
+    df = source_mod_1D.fv5p(Qx,P)[2]
+    assert np.allclose(df,dfdx)
+    assert np.allclose(df,dfdy)
+
+
+
 #Plottign functions
 def myContour(args):
+    """This funciton generates contours"""
     i,fig,ax1,ax2,ax3,xgrid,ygrid,ddata,sdata=args
     ax1.contourf(xgrid,ygrid,sdata[i,:,:],levels=20,cmap=cm.inferno)
     ax2.contourf(xgrid,ygrid,ddata[i,:,:],levels=20,cmap=cm.inferno)
@@ -207,7 +373,7 @@ def set_lims(fig,axes):
     lim1 = 300*np.ones(3)
     lim2 = 375*np.ones(3)
     lim1[2] = 0
-    lim2[2] = 1e-10
+    lim2[2] = 1
     for i,ax in enumerate(axes):
         ax.set_ylim(-5, 5)
         ax.set_xlim(-5, 5)
@@ -261,4 +427,7 @@ class sweep_lambda(object):
 
 
 if __name__ == "__main__":
-    test_comparison_hde(generate_fig=True)
+    # test_comparison_eqt(generate_fig=False)
+    # test_comparison_vortex(generate_fig=True)
+    test_comparison_hde()
+    # test_flux()