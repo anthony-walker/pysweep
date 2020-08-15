abab6ffc2234357dc1237d57a636c672ceea8aff
Anthony Walker <walkanth@oregonstate.edu>
2020-08-14 22:39:41 -0700

    Debugging

--- pysweep/core/GPUtil.py
+++ pysweep/core/GPUtil.py
@@ -81,6 +81,7 @@ def getGPUs():
         p = Popen([nvidia_smi,"--query-gpu=index,uuid,utilization.gpu,memory.total,memory.used,memory.free,driver_version,name,gpu_serial,display_active,display_mode,temperature.gpu", "--format=csv,noheader,nounits"], stdout=PIPE)
         stdout, stderror = p.communicate()
     except:
+        print("returning None")
         return []
     output = stdout.decode('UTF-8')
     print(output)
