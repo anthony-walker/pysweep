24504167b56435045fe72c00877f73a04b4f3e4a
Anthony Walker <walkanth@oregonstate.edu>
2020-08-14 22:22:20 -0700

    Fix no devices found of GPUtil.py

--- pysweep/core/GPUtil.py
+++ pysweep/core/GPUtil.py
@@ -90,40 +90,38 @@ def getGPUs():
     lines = output.split(os.linesep)
     #print(lines)
     numDevices = len(lines)-1
-    print(lines)
+    numDevices = 0 if lines[0] == 'No devices were found' else numDevices #Fix no devices found error
     GPUs = []
-    # for g in range(numDevices):
-    #     line = lines[g]
-    #     #print(line)
-    #     vals = line.split(', ')
-    #     #print(vals)
-    #     for i in range(12):
-    #         # print(vals[i])
-    #         if (i == 0):
-    #             deviceIds = int(vals[i])
-    #         elif (i == 1):
-    #             uuid = vals[i]
-    #         elif (i == 2):
-    #             gpuUtil = safeFloatCast(vals[i])/100
-    #         elif (i == 3):
-    #             memTotal = safeFloatCast(vals[i])
-    #         elif (i == 4):
-    #             memUsed = safeFloatCast(vals[i])
-    #         elif (i == 5):
-    #             memFree = safeFloatCast(vals[i])
-    #         elif (i == 6):
-    #             driver = vals[i]
-    #         elif (i == 7):
-    #             gpu_name = vals[i]
-    #         elif (i == 8):
-    #             serial = vals[i]
-    #         elif (i == 9):
-    #             display_active = vals[i]
-    #         elif (i == 10):
-    #             display_mode = vals[i]
-    #         elif (i == 11):
-    #             temp_gpu = safeFloatCast(vals[i]);
-    #     GPUs.append(GPU(deviceIds, uuid, gpuUtil, memTotal, memUsed, memFree, driver, gpu_name, serial, display_mode, display_active, temp_gpu))
+    for g in range(numDevices):
+        line = lines[g]
+        vals = line.split(', ')
+        for i in range(12):
+            # print(vals[i])
+            if (i == 0):
+                deviceIds = int(vals[i])
+            elif (i == 1):
+                uuid = vals[i]
+            elif (i == 2):
+                gpuUtil = safeFloatCast(vals[i])/100
+            elif (i == 3):
+                memTotal = safeFloatCast(vals[i])
+            elif (i == 4):
+                memUsed = safeFloatCast(vals[i])
+            elif (i == 5):
+                memFree = safeFloatCast(vals[i])
+            elif (i == 6):
+                driver = vals[i]
+            elif (i == 7):
+                gpu_name = vals[i]
+            elif (i == 8):
+                serial = vals[i]
+            elif (i == 9):
+                display_active = vals[i]
+            elif (i == 10):
+                display_mode = vals[i]
+            elif (i == 11):
+                temp_gpu = safeFloatCast(vals[i]);
+        GPUs.append(GPU(deviceIds, uuid, gpuUtil, memTotal, memUsed, memFree, driver, gpu_name, serial, display_mode, display_active, temp_gpu))
     return GPUs  # (deviceIds, gpuUtil, memUtil)
 
 
