d45afd4d6bcef302f85661382f7db2e36c9854b9
Anthony Walker <walkanth@oregonstate.edu>
2020-08-19 12:17:36 -0700

    Updated launches to be complete

--- study/newHardware/launch.sh
+++ study/newHardware/launch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-for bs in 20 #8 12 16 20 24 32
+for bs in 8 12 16 20 24 32
 do
     for gs in $(seq 0 0.10 1)
     do
         export PYSWEEP_SHARE=$gs
         export PYSWEEP_BLOCK=$bs
-        sbatch -J "new"$bs$gs new.sh
+        sbatch -J "new"$PYSWEEP_EQN$bs$gs new.sh
     done
 done
--- study/oldHardware/launch.sh
+++ study/oldHardware/launch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-for bs in 20 #8 12 16 20 24 32
+for bs in 8 12 16 20 24 32
 do
     for gs in $(seq 0 0.10 1)
     do
         export PYSWEEP_SHARE=$gs
         export PYSWEEP_BLOCK=$bs
-        sbatch -J "old"$bs$gs old.sh
+        sbatch -J "old"$PYSWEEP_EQN$bs$gs old.sh
     done
 done
