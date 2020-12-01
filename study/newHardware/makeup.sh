07306001e4a7329d482cbf1b8f26002b9487dc32
Anthony Walker <walkanth@oregonstate.edu>
2020-12-01 13:22:02 -0800

    Fixed issues with not running

--- study/newHardware/makeup.sh
+++ study/newHardware/makeup.sh
@@ -36,6 +36,8 @@ for i in 20,0.5 16,0.3;
 do 
     for nx in 160 320 480 640 800 960 1120
     do
+        IFS=","; 
+        set -- $i; 
         export PYSWEEP_SHARE=$2
         export PYSWEEP_BLOCK=$1
         export PYSWEEP_ARRSIZE=$nx
--- study/oldHardware/makeup.sh
+++ study/oldHardware/makeup.sh
@@ -35,7 +35,9 @@ export PYSWEEP_EQN=euler
 for i in 8,0.8 12,0 12,0.1 12,0.2 12,0.3 12,0.4 16,0 24,0; 
 do 
     for nx in 160 320 480 640 800 960 1120
-    do
+    do  
+        IFS=","; 
+        set -- $i; 
         export PYSWEEP_SHARE=$2
         export PYSWEEP_BLOCK=$1
         export PYSWEEP_ARRSIZE=$nx
