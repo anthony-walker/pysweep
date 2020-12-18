0822d3df8dea0de8559b5a4fb4858a9035952ab5
Anthony Walker <walkanth@submit-a.hpc.engr.oregonstate.edu>
2020-12-18 11:08:48 -0800

    Completed study cases

--- study/newHardware/makeup.sh
+++ study/newHardware/makeup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 export PYSWEEP_EQN=euler
 
-for i in 20,0.5 16,0.3; 
+for i in 16,0.2 #20,0.5 16,0.3; 
 do 
-    for nx in 160 320 480 640 800 960 1120
+    for nx in 960 #160 320 480 640 800 960 1120
     do
         IFS=","; 
         set -- $i; 
         export PYSWEEP_SHARE=$2
         export PYSWEEP_BLOCK=$1
         export PYSWEEP_ARRSIZE=$nx
-        sbatch -J "newSw"$PYSWEEP_EQN$1$2$nx sweptm.sh
+        #sbatch -J "newSw"$PYSWEEP_EQN$1$2$nx sweptm.sh
         sbatch -J "newSt"$PYSWEEP_EQN$1$2$nx stdm.sh
     done 
 done
