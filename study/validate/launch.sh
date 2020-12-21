4fe638df412a6b8f7ad641931310bd9efc19644b
Anthony Walker <walkanth@oregonstate.edu>
2020-12-20 18:01:43 -0800

    Fix bash script

--- study/validate/launch.sh
+++ study/validate/launch.sh
@@ -5,12 +5,14 @@ do
     for nx in 1120
     do  
         for eqn in euler heat
-        IFS=","; 
-        set -- $i; 
-        export PYSWEEP_SHARE=$2
-        export PYSWEEP_BLOCK=$1
-        export PYSWEEP_ARRSIZE=$nx
-        export PYSWEEP_EQN=$eqn
-        sbatch -J "val"$PYSWEEP_EQN$1$2$nx validate.sh
+        do
+            IFS=","; 
+            set -- $i; 
+            export PYSWEEP_SHARE=$2
+            export PYSWEEP_BLOCK=$1
+            export PYSWEEP_ARRSIZE=$nx
+            export PYSWEEP_EQN=$eqn
+            sbatch -J "val"$PYSWEEP_EQN$1$2$nx validate.sh
+        done
     done 
 done
