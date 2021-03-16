918f40855e4bf1572dd30b1def11be60519baf6b
Anthony Walker <walkanth@oregonstate.edu>
2021-03-16 16:23:49 -0700

    Fix launch

--- study/scalability/launch.sh
+++ study/scalability/launch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 a=1
-for name in one two three four #five six seven 
+for name in one two three four
 do
     for eqn in heat euler
     do
         export PYSWEEP_NODES=$a
         export PYSWEEP_FILE=$name
         sbatch -N $a --nodefile ./hosts/$name scalability.sh
-        a=$((a+1))
     done
+    a=$((a+1))
 done
