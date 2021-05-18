d0204ecb7155c6cfaa34bd0f7dee2e6924a4b159
Anthony Walker <walkanth@oregonstate.edu>
2021-05-18 14:44:49 -0700

    Update launch scripts

--- study/sierra/multi-gpu/launch.py
+++ study/sierra/multi-gpu/launch.py
@@ -1,9 +1,10 @@
+#!/usr/bin/python
 import math
 batch = """#!/bin/tcsh
     ### myBatchSubmit
     ### LSF syntax
     #BSUB -nnodes {:d}                   #number of nodes
-    #BSUB -W 100                      #walltime in minutes
+    #BSUB -W 72:00                      #walltime in hours:minutes
     #BSUB -G orsu                     #account
     #BSUB -e pysweep-log-{:d}.txt              #stderr
     #BSUB -o pysweep-out-{:d}.txt              #stdout
@@ -21,22 +22,23 @@ batch = """#!/bin/tcsh
     echo 'Time at computation start:'
     date
     echo ' '
-    jsrun -n{:d} -r1 -a40 -c40 pysweep -f euler -nx {:d} -nt 2000 -b 16 -s 1 --swept --verbose --ignore --clean
+    jsrun -n{:d} -r1 -a40 -c40 -g4 pysweep -f euler -nx {:d} -nt 2000 -b 16 -s 1 --swept --verbose --ignore --clean
 
-    jsrun -n{:d} -r1 -a40 -c40 pysweep -f euler -nx {:d} -nt 2000 -b 16 -s 1 --verbose --ignore --clean
+    jsrun -n{:d} -r1 -a40 -c40 -g4 pysweep -f euler -nx {:d} -nt 2000 -b 16 -s 1 --verbose --ignore --clean
 
-    jsrun -n{:d} -r1 -a40 -c40 pysweep -f heat -nx {:d} -nt 2000 -b 16 -s 1 --swept --verbose --ignore --clean
+    jsrun -n{:d} -r1 -a40 -c40 -g4 pysweep -f heat -nx {:d} -nt 2000 -b 16 -s 1 --swept --verbose --ignore --clean
 
-    jsrun -n{:d} -r1 -a40 -c40 pysweep -f heat -nx {:d} -nt 2000 -b 16 -s 1 --verbose --ignore --clean
+    jsrun -n{:d} -r1 -a40 -c40 -g4 pysweep -f heat -nx {:d} -nt 2000 -b 16 -s 1 --verbose --ignore --clean
     echo 'Time at computation finish:'
     date
     echo 'Done'
+    
     """
 
 files = []
-node_numbers = [1,2]+[i for i in range(4,36,4)]
+node_numbers = [1,]+[i for i in range(2,36,2)]
 for i in node_numbers:
-    npts = int(math.sqrt(2000000*i))
+    npts = int(math.sqrt(8000000*i))
     fname =  'pysweep-batch-{:d}'.format(i)
     files.append(fname)   
     with open(fname,'w') as f:
